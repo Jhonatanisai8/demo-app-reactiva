f8b81d52d7fc91b3c627bb97674f3fd87382ee5a
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-22 14:46:28 -0500

    feat: Initialize new Spring Boot reactive application with R2DBC, WebFlux, and MySQL dependencies.

---
+++ src/main/java/com/jhona/app/DemoAppReactivaApplication.java
@@ -0,0 +1,13 @@
+package com.jhona.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppReactivaApplication.class, args);
+	}
+
+}
